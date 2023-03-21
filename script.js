4f445d95f8fb67dadcccf0d3a5f1331540f9c802
ze-antunes <jprantunes2000@gmail.com>
2023-03-21 19:00:31 +0000

    test Google Identity for web

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function handleCredencialResponse(response) {
-    console.log(response)
-}
+window.handleCredentialResponse = (response) => {
+    // decodeJwtResponse() is a custom function defined by you
+    // to decode the credential response.
+    responsePayload = decodeJwtResponse(response.credential);
+  
+    console.log("ID: " + responsePayload.sub);
+    console.log('Full Name: ' + responsePayload.name);
+    console.log('Given Name: ' + responsePayload.given_name);
+    console.log('Family Name: ' + responsePayload.family_name);
+    console.log("Image URL: " + responsePayload.picture);
+    console.log("Email: " + responsePayload.email);
+  }
 
 function decodeJwtResponse(data) {
     let tokens = data.split(".")
